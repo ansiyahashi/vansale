3d8e0a38fcd8790632f29d223d0821c37dcaf0f3
ansiyahashi <ansiyap98@gmail.com>
2020-11-05 16:01:09 +0530

    login

--- app/src/main/java/com/example/mobvansale/Login.java
+++ app/src/main/java/com/example/mobvansale/Login.java
@@ -5,13 +5,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.List;
 
 public class Login extends AppCompatActivity {
     private Button btnsignup;
     private Button btnlogin;
-
+EditText username;
+    EditText password;
+    List<RegistrationModel> registrationModels;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +26,8 @@ public class Login extends AppCompatActivity {
         DatabaseHandler databaseHandler = DatabaseHandler.getInstance(getApplicationContext());
         btnsignup = (Button) findViewById(R.id.bsignup);
         btnlogin = (Button) findViewById(R.id.login);
-
+        username=(EditText) findViewById(R.id.email);
+        password=(EditText) findViewById(R.id.password);
 
         btnsignup.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,7 +39,44 @@ public class Login extends AppCompatActivity {
         btnlogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                login();
+
+
+                if(username.getText().toString().equalsIgnoreCase("a") && password.getText().toString().equalsIgnoreCase("a")) {
+                    login();
+                }
+else {
+
+                    registration registration=new registration(getApplicationContext());
+                    RegistrationModel registrationModel= registration.selectdata(username.getText().toString());
+                    if (registrationModel==null) {
+                        Toast.makeText(getApplicationContext(), "No user found", Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        if(password.getText().toString().equalsIgnoreCase( registrationModel.getPassword())){
+                            Intent intent = new Intent(Login.this, Homesalesman.class);
+                            startActivity(intent);
+                        }
+                        else {
+                            Toast.makeText(getApplicationContext(), "Invalid username or password", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                }
+
+
+                //Routeinsertion routeinsertion = new Routeinsertion(this);
+
+//
+
+
+
+
+
+
+
+
+
+                //login();
             }
         });
     }
